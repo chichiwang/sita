c4ad5a5e0d1105a2b150937f8e3687baf03870cc
Chi-chi Wang <chichi@wangchiyi.com>
2017-11-16 21:24:33 -0600

    [eslint] Add eslint to the project

--- src/index.js
+++ src/index.js
@@ -4,9 +4,9 @@ import chalk from 'chalk';
 const server = new Koa();
 
 server.use(async function catchAll(ctx) {
-  ctx.body = 'Hello Koa!';
+  ctx.body = 'Hello Koa!'; // eslint-disable-line fp/no-mutation
 });
 
 const msgServerStart = chalk.green.bgBlack('[sita] Server starting on port 3000...');
-console.log(msgServerStart);
+console.log(msgServerStart); // eslint-disable-line no-console
 server.listen(3000);
