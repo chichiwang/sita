df7b7ba3773f030e145dc60ca9214ccb64f4596a
Chi-chi Wang <chichi@wangchiyi.com>
2017-11-16 21:40:18 -0600

    [prettier] Add [prettier](https://github.com/prettier/prettier) to the project

--- src/index.js
+++ src/index.js
@@ -8,6 +8,8 @@ server.use(async function catchAll(ctx) {
   ctx.body = 'Hello Koa!'; // eslint-disable-line fp/no-mutation
 });
 
-const msgServerStart: string = chalk.green.bgBlack('[sita] Server starting on port 3000...');
+const msgServerStart: string = chalk.green.bgBlack(
+  '[sita] Server starting on port 3000...',
+);
 console.log(msgServerStart); // eslint-disable-line no-console
 server.listen(3000);
