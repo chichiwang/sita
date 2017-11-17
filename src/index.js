c883e26e4053d188f52588cf7d72e30867f48266
Chi-chi Wang <chichi@wangchiyi.com>
2017-11-16 21:36:51 -0600

    [flow] Add [flow](https://github.com/facebook/flow) to the project

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
+/* @flow */
 import Koa from 'koa';
 import chalk from 'chalk';
 
-const server = new Koa();
+const server: Object = new Koa();
 
 server.use(async function catchAll(ctx) {
   ctx.body = 'Hello Koa!'; // eslint-disable-line fp/no-mutation
 });
 
-const msgServerStart = chalk.green.bgBlack('[sita] Server starting on port 3000...');
+const msgServerStart: string = chalk.green.bgBlack('[sita] Server starting on port 3000...');
 console.log(msgServerStart); // eslint-disable-line no-console
 server.listen(3000);
