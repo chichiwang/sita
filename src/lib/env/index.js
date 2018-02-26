0d7fa538c5030bd647f5d49bf1c96a4a065b3817
Chi-chi Wang <chichi@wangchiyi.com>
2018-02-26 16:55:09 -0600

    [lib:env] Scaffold in server-side implementation of env variable retrieval

---
+++ src/lib/env/index.js
@@ -0,0 +1,13 @@
+/* @flow */
+import { isBrowser } from 'lib/runtime';
+
+const defaults = {
+  NODE_ENV: 'local',
+};
+
+export default function env(envVar: string): string | void {
+  if (isBrowser) return undefined; // TODO: Retrieve from API endpoint or global object if browser
+
+  const envVal = process.env[envVar];
+  return envVal === 'undefined' ? defaults[envVar] : envVal;
+};
